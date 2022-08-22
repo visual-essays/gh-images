5ae70491251fa1d31342c3034becd92743d59e29
Ron Snyder <ron@snyderjr.com>
2022-08-21 22:22:17 -0400

    theme color test

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,8 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
-      { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' }
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+      { name: 'theme-color', content: '#014473' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
